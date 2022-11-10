834c1308d58b5f178ecb6b8003c404136977bc97
saaidin <dinme73@gmail.com>
2022-11-11 04:59:35 +0800

    rebuild website

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Main from "../components/Main";
 import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
@@ -11,11 +16,11 @@ export default function Home() {
         <link rel="icon" href="/fav.png" />
       </Head>
       <Navbar/>
-      {/* <Main /> */}
-      {/* <About /> */}
-      {/* <Skills /> */}
-      {/* <Projects /> */}
-      {/* <Contact /> */}
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   ) 
 }    
